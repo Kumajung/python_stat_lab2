a2bcb9a8641128ab4d246eac81ab0683d43fce19
Kumajung <kaidohjung@gmail.com>
2024-11-28 15:19:05 +0700

    Update part1_ex5.py

--- part1_ex5.py
+++ part1_ex5.py
@@ -1,13 +1,21 @@
-# ตัวอย่างกราฟวงกลม ข้อมูลจากตัวอย่างที่ 1
 import matplotlib.pyplot as plt
 from collections import Counter
+
 # ข้อมูลสีของนักเรียน
-colors = colors = ["Red", "Blue", "Purple", "Red", "Pink", "Pink", "Yellow", "Pink",
+colors = ["Red", "Blue", "Purple", "Red", "Pink", "Pink", "Yellow", "Pink",
           "Blue", "Green", "Blue", "Pink", "Purple", "Green", "Blue",
           "Blue", "Yellow", "Green", "Pink", "Yellow"]
 color_counts = Counter(colors)
 
+# ตั้งค่าสี (เลือกสีตามความเหมาะสม)
+custom_colors = ["#FF9999", "#66B2FF", "#CC99FF", "#FFCC99", "#FFFF99", "#99FF99"]
+
 # สร้างกราฟวงกลม
-plt.pie(color_counts.values(), labels=color_counts.keys(), autopct='%1.1f%%')
+plt.pie(
+    color_counts.values(),
+    labels=color_counts.keys(),
+    autopct='%1.1f%%',
+    colors=custom_colors
+)
 plt.title("Students' favorite colors")
 plt.show()
